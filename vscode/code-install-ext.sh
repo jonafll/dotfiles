aacfc395d3d84626455417d5b9065d3ed0c4d28c
jonafll <contact@jonafellisch.com>
2021-11-01 16:41:29 +0100

    Add vscode extensions and settings

---
+++ vscode/code-install-ext.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+IFS=$'\n'
+RED=$'\e[1;31m'
+NC=$'\e[0m'
+
+if ! [ -d ~/Library/Application\ Support/Code ]; then
+  echo "${RED}Error:${NC} Visual Studio Code is not installed"
+  exit 1
+fi
+
+if ! [ -f /usr/local/bin/code ]; then
+  echo "Install code command in path"
+  ln -s /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code /usr/local/bin/code
+fi
+
+while IFS= read -r line; do
+  code --install-extension "$line"
+done < ~/.dotfiles/vscode/extensions.txt
